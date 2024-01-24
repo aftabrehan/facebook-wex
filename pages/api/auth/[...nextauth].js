68fa28f4a542eaf2fb6fdc4729d60dee841f02d8
aftabrehan <aftabrehan52422@gmail.com>
2024-01-24 21:37:14 +0500

    Add google sign in

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth'
 import FacebookProviders from 'next-auth/providers/facebook'
-// import GoogleProviders from 'next-auth/providers/google'
+import GoogleProviders from 'next-auth/providers/google'
 
 export default NextAuth({
   providers: [
@@ -8,9 +8,9 @@ export default NextAuth({
       clientId: process.env.FACEBOOK_CLIENT_ID,
       clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
-    // GoogleProviders({
-    //   clientId: process.env.GOOGLE_CLIENT_ID,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    // }),
+    GoogleProviders({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
 })
